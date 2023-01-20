4d75fb89c0e30849b36889dd294a7b7088e8ad74
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2023-01-20 16:57:40 +0100

    add reflection sample

---
+++ reflection/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<ReflectionBenchmarks>();
+
+[SimpleJob(RuntimeMoniker.Net60)]
+[SimpleJob(RuntimeMoniker.Net70)]
+[MemoryDiagnoser(false)]
+public class ReflectionBenchmarks
+{
+    private MethodInfo _method;
+    private static void MyMethod() { }
+
+    [GlobalSetup]
+    public void Setup() => _method = typeof(ReflectionBenchmarks).GetMethod(nameof(MyMethod), BindingFlags.NonPublic | BindingFlags.Static);
+
+    [Benchmark]
+    public void MethodInfoInvoke() => _method.Invoke(null, null);
+
+    [Benchmark]
+    public Type GetUnderlyingType() => Enum.GetUnderlyingType(typeof(DayOfWeek));
+}
